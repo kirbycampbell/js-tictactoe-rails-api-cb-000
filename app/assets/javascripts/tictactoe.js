d3508d4adfdc3b0242a52a3d533a8836f5400f08
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-30 21:56:52 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,8 @@
 // Code your JavaScript / jQuery solution here
 
+const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
 function player() {
   doTurn();
   //Not sure why the test requires this in reverse for it to work
