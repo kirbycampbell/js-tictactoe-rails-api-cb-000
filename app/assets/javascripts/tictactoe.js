db3ad66fdeda2d0832cb21622bfec373e63b5a5d
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-30 21:26:09 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,16 @@
 // Code your JavaScript / jQuery solution here
+
+function player() {
+    if (turn() % 2 ===0){
+        console.log("its even")
+    }
+    else {
+        console.log("its odd")
+    }
+}
+
+let num = 0;
+
+function turn() {
+  return num++;
+}
