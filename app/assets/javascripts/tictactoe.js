e0049d553781977996a52cae807afb93c48dc399
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-30 22:58:06 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -47,6 +47,29 @@ function checkWinner() {
   return winner;
 }
 
+function saveGame() {
+  var state = [];
+  var gameData;
+  $('td').text((index, square) => {
+    state.push(square);
+  });
+
+  gameData = { state: state };
+  if (currentGame) {
+    $.ajax({
+      type: 'PATCH',
+      url: `/games/${currentGame}`,
+      data: gameData
+    });
+  } else {
+    $.post('/games', gameData, function(game) {
+      currentGame = game.data.id;
+      $('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`);
+      $("#gameid-" + game.data.id).on('click', () => reloadGame(game.data.id));
+    });
+  }
+}
+
 function updateState(spot) {
   spot.innerHTML = player();
 }
